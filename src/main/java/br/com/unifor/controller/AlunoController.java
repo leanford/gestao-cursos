f6d6e96f5e3083ddf5989772ff0992f26497bb5a
leanford <leandroalvesmaciel@outlook.com>
2025-04-02 22:50:04 -0300

    feat: adicionando controllers

--- src/main/java/br/com/unifor/controller/AlunoController.java
+++ src/main/java/br/com/unifor/controller/AlunoController.java
@@ -1,4 +1,43 @@
 package br.com.unifor.controller;
 
+import br.com.unifor.entity.Aluno;
+import br.com.unifor.service.AlunoService;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+
+import org.jboss.logging.Logger;
+
+@Path("/alunos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AlunoController {
+    @Inject
+    AlunoService alunoService;
+
+    private static final Logger LOG = Logger.getLogger(AlunoController.class);
+
+    @GET
+    public List<Aluno> listar() { return alunoService.listar(); }
+
+    @GET
+    @Path("/{id}")
+    public Aluno buscar(@PathParam("id") Long id) { return alunoService.buscar(id); }
+
+    @POST
+    public Response criar(Aluno aluno) {
+        alunoService.criar(aluno);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deletar(@PathParam("id") Long id) {
+        alunoService.deletar(id);
+        return Response.noContent().build();
+    }
 }
--- src/main/java/br/com/unifor/controller/CursoController.java
+++ src/main/java/br/com/unifor/controller/CursoController.java
@@ -1,4 +1,39 @@
 package br.com.unifor.controller;
 
-public class CursoController {
+import br.com.unifor.entity.Curso;
+import br.com.unifor.service.CursoService;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/cursos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+class CursoController {
+    @Inject
+    CursoService cursoService;
+
+    @GET
+    public List<Curso> listar() { return cursoService.listar(); }
+
+    @GET
+    @Path("/{id}")
+    public Curso buscar(@PathParam("id") Long id) { return cursoService.buscar(id); }
+
+    @POST
+    public Response criar(Curso curso) {
+        cursoService.criar(curso);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deletar(@PathParam("id") Long id) {
+        cursoService.deletar(id);
+        return Response.noContent().build();
+    }
 }
