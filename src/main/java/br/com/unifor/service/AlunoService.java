35da792814d26dbc19bef255b77c9c419158bec0
leanford <leandroalvesmaciel@outlook.com>
2025-04-02 22:52:26 -0300

    feat: iniciando services

--- src/main/java/br/com/unifor/service/AlunoService.java
+++ src/main/java/br/com/unifor/service/AlunoService.java
@@ -1,4 +1,36 @@
 package br.com.unifor.service;
 
+import br.com.unifor.entity.Aluno;
+import br.com.unifor.repository.AlunoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.NotFoundException;
+
+import java.util.List;
+
+@ApplicationScoped
+@Transactional
 public class AlunoService {
+    @Inject
+    AlunoRepository alunoRepository;
+
+    public List<Aluno> listar() { return alunoRepository.listAll(); }
+    public Aluno buscar(Long id) { return alunoRepository.findById(id); }
+    public void criar(Aluno aluno) { alunoRepository.persist(aluno); }
+
+    @Transactional
+    public void deletar(Long id) {
+        System.out.println("Aluno ID: " + id);
+
+        Aluno aluno = alunoRepository.findById(id);
+        if (aluno == null) {
+            throw new NotFoundException("Aluno não encontrado com ID: " + id);
+        }
+        System.out.println("Aluno ID: " + id);
+
+        alunoRepository.delete(aluno);
+    }
+
+
 }
--- src/main/java/br/com/unifor/service/CursoService.java
+++ src/main/java/br/com/unifor/service/CursoService.java
@@ -1,4 +1,26 @@
 package br.com.unifor.service;
 
+
+import br.com.unifor.entity.Curso;
+import br.com.unifor.repository.CursoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@ApplicationScoped
+@Transactional
 public class CursoService {
+    @Inject
+    CursoRepository cursoRepository;
+
+    public List<Curso> listar() { return cursoRepository.listAll(); }
+    public Curso buscar(Long id) { return cursoRepository.findById(id); }
+
+
+    public void criar(Curso curso) { cursoRepository.persist(curso); }
+
+    @Transactional
+    public void deletar(Long id) { cursoRepository.deleteById(id); }
 }
