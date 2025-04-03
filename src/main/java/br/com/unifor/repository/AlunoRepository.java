a5084c5c37ec23235ca719aac67effee386c0623
leanford <leandroalvesmaciel@outlook.com>
2025-04-02 22:51:29 -0300

    Criação de repositories

--- src/main/java/br/com/unifor/repository/AlunoRepository.java
+++ src/main/java/br/com/unifor/repository/AlunoRepository.java
@@ -1,4 +1,9 @@
 package br.com.unifor.repository;
 
-public class AlunoRepository {
-}
+
+import br.com.unifor.entity.Aluno;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class AlunoRepository implements PanacheRepository<Aluno> {}
--- src/main/java/br/com/unifor/repository/CursoRepository.java
+++ src/main/java/br/com/unifor/repository/CursoRepository.java
@@ -1,4 +1,9 @@
 package br.com.unifor.repository;
 
-public class CursoRepository {
-}
+
+import br.com.unifor.entity.Curso;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CursoRepository implements PanacheRepository<Curso> {}
